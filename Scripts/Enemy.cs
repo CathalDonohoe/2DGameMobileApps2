65cf4f241dccc71ac59a3b8a1babb6f719d23aec
CATHAL DONOHOE - STUDENT <G00344919@gmit.ie>
2020-02-10 15:04:19 +0000

    first commit

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float distance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
+        if (hitInfo.collider != null)
+        {
+            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
+        }
+        else {
+
+            Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
+        }
+    }
+}
